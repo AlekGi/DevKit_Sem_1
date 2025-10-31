dbb8be216eee0701e124467c41f97a67556bb6b2
AlekGi <alekrash@gmail.com>
2025-10-31 14:38:15 +0300

    Initial Commit

---
+++ src/main/java/hw/task1/GameWindow.java
@@ -0,0 +1,57 @@
+package hw.task1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameWindow extends JFrame {
+    private static final int WINDOW_HEIGHT = 555;
+    private static final int WINDOW_WIDTH = 507;
+    //private static final int WINDOW_POSX = 800;
+    //private static final int WINDOW_POSY = 300;
+
+    private JButton btnStart, btnExit;
+    private SettingsWindow settings;
+    private Map map;
+
+    GameWindow() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //setLocation(WINDOW_POSX, WINDOW_POSY);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle("Крестики-нолики");
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        btnStart = new JButton("Играть");
+        btnExit = new JButton("Выход");
+
+        map = new Map();
+        settings = new SettingsWindow(this);
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                settings.setVisible(true);
+            }
+        });
+
+
+
+        JPanel panBottom = new JPanel(new GridLayout(1,2));
+        panBottom.add(btnStart);
+        panBottom.add(btnExit);
+        add(panBottom, BorderLayout.SOUTH);
+        add(map);
+        setVisible(true);
+    }
+
+    void startNewGame(int mode, int fSzX, int fSzY, int wLen){
+        map.startNewGame(mode,  fSzX,  fSzY,  wLen);
+    }
+}
---
+++ src/main/java/hw/task1/SettingsWindow.java
@@ -0,0 +1,28 @@
+package hw.task1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SettingsWindow extends JFrame {
+    private static final int WINDOW_HEIGHT = 230;
+    private static final int WINDOW_WIDTH = 350;
+
+    private JButton btnStart;
+
+    SettingsWindow(GameWindow gameWindow){
+        btnStart = new JButton("Старт новой игры");
+        setLocationRelativeTo(gameWindow);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameWindow.startNewGame(0,3,3,3);
+                setVisible(false);
+            }
+        });
+        add(btnStart);
+    }
+
+
+}
